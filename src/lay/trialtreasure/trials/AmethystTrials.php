7b9f5007aa32a41fd98d673e5371de8fef42aaae
LayWasTaken <clarencetoledo08@gmail.com>
2024-06-25 18:02:47 +0800

    Updated Requirements

--- src/lay/trialtreasure/trials/AmethystTrials.php
+++ src/lay/trialtreasure/trials/AmethystTrials.php
@@ -7,47 +7,26 @@ use Lay\TrialTreasure\EnemyWaves;
 use Lay\TrialTreasure\entity\enemies\Skeleton;
 use Lay\TrialTreasure\entity\enemies\Zombie;
 use Lay\TrialTreasure\TrialTreasure;
-use pocketmine\entity\Location;
 use pocketmine\item\VanillaItems;
-use pocketmine\player\Player;
-use pocketmine\world\Position;
 
 class AmethystTrials extends TrialTreasure implements EnemyWaves {
 
-    public static function getWaves(Player $player, Position $origin, int $difficulty): Generator{
-        $location1 = Location::fromObject($origin->add(0, 1, 3), $origin->world, mt_rand(0, 35) * 100, 0);
-        $location2 = Location::fromObject($origin->add(0, 1, -3), $origin->world, mt_rand(0, 35) * 100, 0);
-        $location3 = Location::fromObject($origin->add(3, 1, 0), $origin->world, mt_rand(0, 35) * 100, 0);
-        $location4 = Location::fromObject($origin->add(-3, 1, 0), $origin->world, mt_rand(0, 35) * 100, 0);
-        $location5 = Location::fromObject($origin->add(3, 1, 1), $origin->world, mt_rand(0, 35) * 100, 0);
-        yield [
-            Skeleton::create($location1, $origin),
-            Skeleton::create($location2, $origin),
-            Zombie::create($location3, $origin),
-            Zombie::create($location4, $origin),
-            Zombie::create($location5, $origin)
-        ];
-        yield [
-            Zombie::create($location1, $origin),
-            Zombie::create($location2, $origin),
-            Skeleton::create($location3, $origin),
-            Skeleton::create($location4, $origin),
-            Skeleton::create($location5, $origin)
-        ];
-        yield [
-            Skeleton::create($location1, $origin),
-            Skeleton::create($location2, $origin),
-            Skeleton::create($location3, $origin),
-            Skeleton::create($location4, $origin),
-            Skeleton::create($location5, $origin)
-        ];
-        yield [
-            Zombie::create($location1, $origin),
-            Zombie::create($location2, $origin),
-            Zombie::create($location3, $origin),
-            Zombie::create($location4, $origin),
-            Zombie::create($location5, $origin)
-        ];
+    public function getWaves(): Generator{
+        yield Skeleton::create($this->getRandomSafeSpawn(), $this);
+        yield Zombie::create($this->getRandomSafeSpawn(), $this);
+        yield Skeleton::create($this->getRandomSafeSpawn(), $this);
+        yield Zombie::create($this->getRandomSafeSpawn(), $this);
+        yield Skeleton::create($this->getRandomSafeSpawn(), $this);
+        yield Zombie::create($this->getRandomSafeSpawn(), $this);
+        yield Skeleton::create($this->getRandomSafeSpawn(), $this);
+        yield Zombie::create($this->getRandomSafeSpawn(), $this);
+        yield Zombie::create($this->getRandomSafeSpawn(), $this);
+        yield Zombie::create($this->getRandomSafeSpawn(), $this);
+        yield Zombie::create($this->getRandomSafeSpawn(), $this);
+        yield Skeleton::create($this->getRandomSafeSpawn(), $this);
+        yield Skeleton::create($this->getRandomSafeSpawn(), $this);
+        yield Skeleton::create($this->getRandomSafeSpawn(), $this);
+        yield Zombie::create($this->getRandomSafeSpawn(), $this);
     }
 
     protected function getRewards(): array{
@@ -59,6 +38,10 @@ class AmethystTrials extends TrialTreasure implements EnemyWaves {
 
     protected static function getWaveClass(): string{
         return self::class;
+    }
+
+    protected function getMaxActiveEnemies(): int{
+        return 3;
     }
 
 }
